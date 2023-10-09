4c04ec0d137b893a3edc4cb773dcdbcebeb07916
Barely-Awake <80858965+Barely-Awake@users.noreply.github.com>
2023-10-08 21:17:29 -0700

    chore: temporarily disable posting data

--- src/routes/players.ts
+++ src/routes/players.ts
@@ -88,6 +88,7 @@ playersRouter.post(
     requirePermissions: ['write'],
   }),
   async (req, res) => {
+    return res.sendStatus(500);
     const updates: PlayerUpdates | unknown = req.body;
     const receivedAt = Date.now();
 
